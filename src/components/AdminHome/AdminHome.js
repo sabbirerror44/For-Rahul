115745f7c8f733e368493ab289b551f29ef281dd
Md. Sabbir Hossen <sabbirerror44@gmail.com>
2021-06-21 20:29:14 +0600

    first commit

---
+++ src/components/AdminHome/AdminHome.js
@@ -0,0 +1,169 @@
+import React from 'react';
+import './AdminHome.css';
+const AdminHome = () => {
+    return (
+        <div className="container">
+            <h2 className="text-white headingStyle">Admin Home Page</h2>
+                <div className="row">
+                     <div className="card col-lg-2 col-md-4 col-sm-6 m-3 Style">
+                        <div className="card-body">
+                        <h5 className="card-title textStyle">Card title</h5>
+                        <p className="card-text">This is a golden opportunity for IT experts</p>
+                        <p className="card-title text-primary">Category: IT</p>
+                        
+                        <select id="status" className="mb-2">
+                            <option value="pending">Pending</option>
+                            <option value="approve">Approve</option>
+                
+                        </select>
+                      <input type="submit" className="btn btn-success" value="Submit" />
+                      
+                        
+                        {/* <button className="btn btn-success">Apply</button> */}
+
+                     </div>
+                     </div>
+
+                     <div className="card col-lg-2 col-md-4 col-sm-6 m-3 Style">
+                        <div className="card-body">
+                        <h5 className="card-title textStyle">Card title</h5>
+                        <p className="card-text">This is a golden opportunity for IT experts</p>
+                        <p className="card-title text-primary">Category: IT</p>
+                        <button className="btn btn-success">Apply</button>
+
+                     </div>
+                     </div>
+                     <div className="card col-lg-2 col-md-4 col-sm-6 m-3 Style">
+                        <div className="card-body">
+                        <h5 className="card-title textStyle">Card title</h5>
+                        <p className="card-text">This is a golden opportunity for IT experts</p>
+                        <p className="card-title text-primary">Category: IT</p>
+                        <button className="btn btn-success">Apply</button>
+
+                     </div>
+                     </div>
+                     <div className="card col-lg-2 col-md-4 col-sm-6 m-3 Style">
+                        <div className="card-body">
+                        <h5 className="card-title textStyle">Card title</h5>
+                        <p className="card-text">This is a golden opportunity for IT experts</p>
+                        <p className="card-title text-primary">Category: IT</p>
+                        <button className="btn btn-success">Apply</button>
+
+                     </div>
+                     </div>
+                     <div className="card col-lg-2 col-md-4 col-sm-6 m-3 Style">
+                        <div className="card-body">
+                        <h5 className="card-title textStyle">Card title</h5>
+                        <p className="card-text">This is a golden opportunity for IT experts</p>
+                        <p className="card-title text-primary">Category: IT</p>
+                        <button className="btn btn-success">Apply</button>
+
+                     </div>
+                     </div>
+                     <div className="card col-lg-2 col-md-4 col-sm-6 m-3 Style">
+                        <div className="card-body">
+                        <h5 className="card-title textStyle">Card title</h5>
+                        <p className="card-text">This is a golden opportunity for IT experts</p>
+                        <p className="card-title text-primary">Category: IT</p>
+                        <button className="btn btn-success">Apply</button>
+
+                     </div>
+                     </div>
+                     <div className="card col-lg-2 col-md-4 col-sm-6 m-3 Style">
+                        <div className="card-body">
+                        <h5 className="card-title textStyle">Card title</h5>
+                        <p className="card-text">This is a golden opportunity for IT experts</p>
+                        <p className="card-title text-primary">Category: IT</p>
+                        <button className="btn btn-success">Apply</button>
+
+                     </div>
+                     </div>
+                     <div className="card col-lg-2 col-md-4 col-sm-6 m-3 Style">
+                        <div className="card-body">
+                        <h5 className="card-title textStyle">Card title</h5>
+                        <p className="card-text">This is a golden opportunity for IT experts</p>
+                        <p className="card-title text-primary">Category: IT</p>
+                        <button className="btn btn-success">Apply</button>
+
+                     </div>
+                     </div>
+                     <div className="card col-lg-2 col-md-4 col-sm-6 m-3 Style">
+                        <div className="card-body">
+                        <h5 className="card-title textStyle">Card title</h5>
+                        <p className="card-text">This is a golden opportunity for IT experts</p>
+                        <p className="card-title text-primary">Category: IT</p>
+                        <button className="btn btn-success">Apply</button>
+
+                     </div>
+                     </div>
+                     <div className="card col-lg-2 col-md-4 col-sm-6 m-3 Style">
+                        <div className="card-body">
+                        <h5 className="card-title textStyle">Card title</h5>
+                        <p className="card-text">This is a golden opportunity for IT experts</p>
+                        <p className="card-title text-primary">Category: IT</p>
+                        <button className="btn btn-success">Apply</button>
+
+                     </div>
+                     </div>
+                     <div className="card col-lg-2 col-md-4 col-sm-6 m-3 Style">
+                        <div className="card-body">
+                        <h5 className="card-title textStyle">Card title</h5>
+                        <p className="card-text">This is a golden opportunity for IT experts</p>
+                        <p className="card-title text-primary">Category: IT</p>
+                        <button className="btn btn-success">Apply</button>
+
+                     </div>
+                     </div>
+                     <div className="card col-lg-2 col-md-4 col-sm-6 m-3 Style">
+                        <div className="card-body">
+                        <h5 className="card-title textStyle">Card title</h5>
+                        <p className="card-text">This is a golden opportunity for IT experts</p>
+                        <p className="card-title text-primary">Category: IT</p>
+                        <button className="btn btn-success">Apply</button>
+
+                     </div>
+                     </div>
+                     <div className="card col-lg-2 col-md-4 col-sm-6 m-3 Style">
+                        <div className="card-body">
+                        <h5 className="card-title textStyle">Card title</h5>
+                        <p className="card-text">This is a golden opportunity for IT experts</p>
+                        <p className="card-title text-primary">Category: IT</p>
+                        <button className="btn btn-success">Apply</button>
+
+                     </div>
+                     </div>
+                     <div className="card col-lg-2 col-md-4 col-sm-6 m-3 Style">
+                        <div className="card-body">
+                        <h5 className="card-title textStyle">Card title</h5>
+                        <p className="card-text">This is a golden opportunity for IT experts</p>
+                        <p className="card-title text-primary">Category: IT</p>
+                        <button className="btn btn-success">Apply</button>
+
+                     </div>
+                     </div>
+                     <div className="card col-lg-2 col-md-4 col-sm-6 m-3 Style">
+                        <div className="card-body">
+                        <h5 className="card-title textStyle">Card title</h5>
+                        <p className="card-text">This is a golden opportunity for IT experts</p>
+                        <p className="card-title text-primary">Category: IT</p>
+                        <button className="btn btn-success">Apply</button>
+
+                     </div>
+                     </div>   <div className="card col-lg-2 col-md-4 col-sm-6 m-3 Style">
+                        <div className="card-body">
+                        <h5 className="card-title textStyle">Card title</h5>
+                        <p className="card-text">This is a golden opportunity for IT experts</p>
+                        <p className="card-title text-primary">Category: IT</p>
+                        <button className="btn btn-success">Apply</button>
+
+                     </div>
+                     </div>
+                    
+              </div>
+                   
+            </div>
+
+    );
+};
+
+export default AdminHome;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,160 @@
+import React from 'react';
+import './Home.css';
+const Home = () => {
+    return (
+        <div className="container">
+            <h2 className="text-white headingStyle">Grab Your Desired Job</h2>
+                <div className="row">
+                     <div className="card col-lg-2 col-md-4 col-sm-6 m-3 Style">
+                        <div className="card-body">
+                        <h5 className="card-title textStyle">Card title</h5>
+                        <p className="card-text">This is a golden opportunity for IT experts</p>
+                        <p className="card-title text-primary">Category: IT</p>
+                        <button className="btn btn-success">Apply</button>
+
+                     </div>
+                     </div>
+
+                     <div className="card col-lg-2 col-md-4 col-sm-6 m-3 Style">
+                        <div className="card-body">
+                        <h5 className="card-title textStyle">Card title</h5>
+                        <p className="card-text">This is a golden opportunity for IT experts</p>
+                        <p className="card-title text-primary">Category: IT</p>
+                        <button className="btn btn-success">Apply</button>
+
+                     </div>
+                     </div>
+                     <div className="card col-lg-2 col-md-4 col-sm-6 m-3 Style">
+                        <div className="card-body">
+                        <h5 className="card-title textStyle">Card title</h5>
+                        <p className="card-text">This is a golden opportunity for IT experts</p>
+                        <p className="card-title text-primary">Category: IT</p>
+                        <button className="btn btn-success">Apply</button>
+
+                     </div>
+                     </div>
+                     <div className="card col-lg-2 col-md-4 col-sm-6 m-3 Style">
+                        <div className="card-body">
+                        <h5 className="card-title textStyle">Card title</h5>
+                        <p className="card-text">This is a golden opportunity for IT experts</p>
+                        <p className="card-title text-primary">Category: IT</p>
+                        <button className="btn btn-success">Apply</button>
+
+                     </div>
+                     </div>
+                     <div className="card col-lg-2 col-md-4 col-sm-6 m-3 Style">
+                        <div className="card-body">
+                        <h5 className="card-title textStyle">Card title</h5>
+                        <p className="card-text">This is a golden opportunity for IT experts</p>
+                        <p className="card-title text-primary">Category: IT</p>
+                        <button className="btn btn-success">Apply</button>
+
+                     </div>
+                     </div>
+                     <div className="card col-lg-2 col-md-4 col-sm-6 m-3 Style">
+                        <div className="card-body">
+                        <h5 className="card-title textStyle">Card title</h5>
+                        <p className="card-text">This is a golden opportunity for IT experts</p>
+                        <p className="card-title text-primary">Category: IT</p>
+                        <button className="btn btn-success">Apply</button>
+
+                     </div>
+                     </div>
+                     <div className="card col-lg-2 col-md-4 col-sm-6 m-3 Style">
+                        <div className="card-body">
+                        <h5 className="card-title textStyle">Card title</h5>
+                        <p className="card-text">This is a golden opportunity for IT experts</p>
+                        <p className="card-title text-primary">Category: IT</p>
+                        <button className="btn btn-success">Apply</button>
+
+                     </div>
+                     </div>
+                     <div className="card col-lg-2 col-md-4 col-sm-6 m-3 Style">
+                        <div className="card-body">
+                        <h5 className="card-title textStyle">Card title</h5>
+                        <p className="card-text">This is a golden opportunity for IT experts</p>
+                        <p className="card-title text-primary">Category: IT</p>
+                        <button className="btn btn-success">Apply</button>
+
+                     </div>
+                     </div>
+                     <div className="card col-lg-2 col-md-4 col-sm-6 m-3 Style">
+                        <div className="card-body">
+                        <h5 className="card-title textStyle">Card title</h5>
+                        <p className="card-text">This is a golden opportunity for IT experts</p>
+                        <p className="card-title text-primary">Category: IT</p>
+                        <button className="btn btn-success">Apply</button>
+
+                     </div>
+                     </div>
+                     <div className="card col-lg-2 col-md-4 col-sm-6 m-3 Style">
+                        <div className="card-body">
+                        <h5 className="card-title textStyle">Card title</h5>
+                        <p className="card-text">This is a golden opportunity for IT experts</p>
+                        <p className="card-title text-primary">Category: IT</p>
+                        <button className="btn btn-success">Apply</button>
+
+                     </div>
+                     </div>
+                     <div className="card col-lg-2 col-md-4 col-sm-6 m-3 Style">
+                        <div className="card-body">
+                        <h5 className="card-title textStyle">Card title</h5>
+                        <p className="card-text">This is a golden opportunity for IT experts</p>
+                        <p className="card-title text-primary">Category: IT</p>
+                        <button className="btn btn-success">Apply</button>
+
+                     </div>
+                     </div>
+                     <div className="card col-lg-2 col-md-4 col-sm-6 m-3 Style">
+                        <div className="card-body">
+                        <h5 className="card-title textStyle">Card title</h5>
+                        <p className="card-text">This is a golden opportunity for IT experts</p>
+                        <p className="card-title text-primary">Category: IT</p>
+                        <button className="btn btn-success">Apply</button>
+
+                     </div>
+                     </div>
+                     <div className="card col-lg-2 col-md-4 col-sm-6 m-3 Style">
+                        <div className="card-body">
+                        <h5 className="card-title textStyle">Card title</h5>
+                        <p className="card-text">This is a golden opportunity for IT experts</p>
+                        <p className="card-title text-primary">Category: IT</p>
+                        <button className="btn btn-success">Apply</button>
+
+                     </div>
+                     </div>
+                     <div className="card col-lg-2 col-md-4 col-sm-6 m-3 Style">
+                        <div className="card-body">
+                        <h5 className="card-title textStyle">Card title</h5>
+                        <p className="card-text">This is a golden opportunity for IT experts</p>
+                        <p className="card-title text-primary">Category: IT</p>
+                        <button className="btn btn-success">Apply</button>
+
+                     </div>
+                     </div>
+                     <div className="card col-lg-2 col-md-4 col-sm-6 m-3 Style">
+                        <div className="card-body">
+                        <h5 className="card-title textStyle">Card title</h5>
+                        <p className="card-text">This is a golden opportunity for IT experts</p>
+                        <p className="card-title text-primary">Category: IT</p>
+                        <button className="btn btn-success">Apply</button>
+
+                     </div>
+                     </div>   <div className="card col-lg-2 col-md-4 col-sm-6 m-3 Style">
+                        <div className="card-body">
+                        <h5 className="card-title textStyle">Card title</h5>
+                        <p className="card-text">This is a golden opportunity for IT experts</p>
+                        <p className="card-title text-primary">Category: IT</p>
+                        <button className="btn btn-success">Apply</button>
+
+                     </div>
+                     </div>
+                    
+              </div>
+                   
+            </div>
+
+    );
+};
+
+export default Home;
